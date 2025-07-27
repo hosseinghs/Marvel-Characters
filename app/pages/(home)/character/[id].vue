2620c9dd6bfabcdb8b7d6fa4d34553f2ca61e571
hossein samani <h.ghobadisamani@starbus.ir>
2025-07-27 17:19:34 +0330

    perf: lazyload components

--- app/pages/(home)/character/[id].vue
+++ app/pages/(home)/character/[id].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import CharactersDetail from '@/components/characters/Detail.vue'
-import CharacterSection from '@/components/common/CharacterSection.vue'
+const CharactersDetail = defineAsyncComponent(() => import('@/components/characters/Detail.vue'))
+const CharacterSection = defineAsyncComponent(() => import('@/components/common/CharacterSection.vue'))
 </script>
 
 <template>
