200df94e939c1b0846d1e1eb6e6130e9d8ec0052
hossein samani <h.ghobadisamani@starbus.ir>
2025-07-27 17:14:16 +0330

    feat: handle loading state

--- app/pages/(home)/index.vue
+++ app/pages/(home)/index.vue
@@ -5,11 +5,12 @@ definePageMeta({
   layout: 'default'
 })
 
-// const { data, error, pending } = await useFetch('/api/marvel/characters?limit=10', {
-//   server: false
-// })
-
 const currentPage = ref(1)
+
+const { pending } = await useFetch('/api/marvel/characters?limit=10', {
+  server: false,
+  watch: [currentPage]
+})
 
 const onCharacterClick = () => {
   navigateTo({
@@ -25,20 +26,28 @@ const onCharacterClick = () => {
 <template>
   <div>
     <div class="grid grid-cols-4 gap-4">
-      <CharactersCard
-        v-for="i in 12"
-        :key="i"
-        class="col-span-12 md:col-span-1"
-        :name="spidermain.name"
-        :image="spidermain.image"
-        @click="onCharacterClick"
-      />
+      <template v-if="pending">
+        loading...
+      </template>
+      <template v-else>
+        <!-- v-for over data -->
+        <CharactersCard
+          v-for="i in 12"
+          :key="i"
+          class="col-span-12 md:col-span-1"
+          :name="spidermain.name"
+          :image="spidermain.image"
+          @click="onCharacterClick"
+        />
+      </template>
+
     </div>
     <div class="text-center">
       <CommonPagination
         v-model:current-page="currentPage"
         :total-pages="10"
         :max-visible="10"
+        :disabled="pending"
       />
     </div>
   </div>
