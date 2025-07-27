8a5468259a0044e3774c2f81016ff97d41e98b69
hossein samani <h.ghobadisamani@starbus.ir>
2025-07-27 16:51:50 +0330

    style: make header component more beatifull

--- app/components/layout/Header.vue
+++ app/components/layout/Header.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+const onLogoClick = () => navigateTo('/')
+</script>
+
 <template>
-  <header>
-    header
+  <header class="bg-gradient-to-l from-[rgba(0,0,0,1)] to-[rgba(90,90,90,1)]">
+    <div>
+      <div class="inline-block px-4 py-2 rounded-lg text-white bg-[#DC2626]" @click="onLogoClick">
+        Logo
+      </div>
+    </div>
   </header>
 </template>
